b4fbaa751ef53d47d16f3c25dd6e6b340e7c2ab7
Alberto <77164768+albynotar@users.noreply.github.com>
2024-09-28 11:26:08 +0200

    updated contract Tris.sol

--- contracts/Tris.sol
+++ contracts/Tris.sol
@@ -5,7 +5,21 @@ import "hardhat/console.sol";
 contract Tris {
     // win event
     event Win(
-        address player
+        address player,
+        string name
+    );
+
+    // draw event
+    event Draw(
+        address player,
+        string name
+    );
+
+    // move event
+    event Move(
+        address player,
+        Value value,
+        uint8 move_count
     );
 
     // enum of single cell
@@ -24,8 +38,6 @@ contract Tris {
     }
 
     //struct Cell for each 8 cells of tris grid
-    Player Player1;
-    Player Player2;
     Cell A1;
     Cell A2;
     Cell A3;
@@ -35,14 +47,32 @@ contract Tris {
     Cell C1;
     Cell C2;
     Cell C3;
-    uint8 move_count;
 
-    // map each cell to an unique identifier
+    // struct for two players
+    Player Player1;
+    Player Player2;
+
+    // address of contract owner
+    address public owner;
+
+    // move state of game
+    uint8 public move_count;
+    
+    /**
+    * @dev set decorator for contract owner_only functions
+    */
+    modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+}
+
+    // map each cell to an unique number
     mapping (uint8 => Cell) public cells;
 
-
     // contract constructor
-    constructor() {
+    constructor(address p1, address p2) {
+
+    // set up all 9 cells as empty cells
     A1 = Cell(Value.NaN, false);
     A2 = Cell(Value.NaN, false);
     A3 = Cell(Value.NaN, false);
@@ -52,28 +82,47 @@ contract Tris {
     C1 = Cell(Value.NaN, false);
     C2 = Cell(Value.NaN, false);
     C3 = Cell(Value.NaN, false);
+
+    // owner is the first caller
+    owner = msg.sender;
+
+    // move count set as 0
     move_count = 0;
+
+    // set players
+    Player1 = Player(p1, Value.X);
+    Player2 = Player(p2, Value.O);
+    }
+
+    /**
+    * @dev function to get the move count
+    */
+    function get_move_count() public view returns (uint8) {
+        return move_count;
     }
     
-    
+    /**
+    * @dev returns state of requested Cell
+    * @param cell_id id of the requested Cell
+    */
     function get_cell(uint8 cell_id) public view returns (Cell memory) {
         return cells[cell_id];
     }
 
-    function set_cell_value(uint8 cell_id, Value user_input) public returns (Cell memory) {
-        cells[cell_id].value = user_input;
-        return cells[cell_id];
-    }
-
+    /**
+    * @dev returns human readable symbol of requested Cell
+    * @param cell_id id of the requested Cell
+    */
     function get_cell_value(uint8 cell_id) public view returns (string memory) {
-        Value strNumber = cells[cell_id].value;
-        if (strNumber == Value.NaN) return "-";
-        if (strNumber == Value.X) return "X";
-        if (strNumber == Value.O) return "O";
-        return "ERROR";
+        Value cell_symbol = cells[cell_id].value;
+        if (cell_symbol == Value.NaN) return "-";
+        if (cell_symbol == Value.X) return "X";
+        if (cell_symbol == Value.O) return "O";
     }
 
-    
+    /**
+    * @dev returns human readable Tris grid with the state of the game
+    */
     function get_grid() public view returns (string memory) {
         string memory grid = string.concat(" _____\n",
         "|",get_cell_value(0), " ", get_cell_value(1)," ", get_cell_value(2),"|\n",
@@ -83,36 +132,78 @@ contract Tris {
         return grid;
     }
 
-    function reset_grid() public {
+    /**
+    * @dev reset the Tris grid
+    */
+    function reset_grid() public onlyOwner {
         for (uint8 i = 0; i <= 8; i++) {
             cells[i] = Cell(Value.NaN, false);
         }
+        move_count = 0;
     }
     
-    
-    function move(address player, uint8 cell_id, Value user_input) public returns (Cell memory) {
-        require(cells[cell_id].assigned == false, 'Cell already assigned');
-        //require(msg.sender.address == , 'Cell already assigned');
-        cells[cell_id].value = user_input;
-        cells[cell_id].assigned = true;
-        move_count = move_count + 1;
-        bool winner_state = check_win();
-        console.log(winner_state);
-        if (winner_state == true){
-            win_state(player);
+    /**
+    * @dev function that let a player make a move
+    * emit a move event for each move and a win event if win condition
+    * is achieved
+    * @param cell_id id of the requested Cell
+    */
+    function move(uint8 cell_id) public returns (bool) {
+        // check that msg.sender is player
+        require(msg.sender == Player1.addr || msg.sender == Player2.addr, 'msg.sender is not an autorized player');
+        // check that cell is not already assigned
+        require(cells[cell_id].assigned == false, 'Cell is already assigned');
+        // check which player has his turn and block illegal moves
+        if (msg.sender == Player1.addr){
+            require(move_count % 2 == 0, 'Player 1 attempted a move outside of his turn');
+            cells[cell_id].value = Player1.symbol;
             }
-        return cells[cell_id];
+        if (msg.sender == Player2.addr){
+            require(move_count % 2 != 0, 'Player 2 attempted a move outside of his turn');
+            cells[cell_id].value = Player2.symbol;
+            }
+        // assign new value to cell with id cell_id
+        cells[cell_id].assigned = true;
+        // emit move event
+        emit Move(msg.sender, cells[cell_id].value, cells[cell_id].ass, move_count);
+        // check win state
+        bool winner_state = check_win();
+        // console.log(winner_state);
+        // if win condition is found emit win event
+        if (winner_state == true){
+            end_state();
+            // emit win event for winner player
+            if (move_count % 2 == 0){
+                emit Win(Player1.addr, "Player1 Win");}
+            else{
+                emit Win(Player2.addr, "Player2 Win");}
+            }
+        // else increase move_count and change turn
+        else{
+            if (move_count == 8){
+                // emit draw event if no more moves are available
+                emit Draw(Player1.addr, "Draw");}
+            else{
+                move_count = move_count + 1;}
+            }
+        // return true if player has win or false if not
+        return winner_state;
     }
 
-    function win_state(address player) public {
+
+    /**
+    * @dev function for declaring th end of the game
+    */
+    function end_state() internal {
+        // block all remaining cells
         for (uint8 i = 0; i <= 8; i++) {
             cells[i].assigned = true;
         }
-        get_grid();
-        emit Win(player);
-        console.log(player);
     }
 
+    /**
+    * @dev function that check if there is a winning configuartion for a player
+    */
     function check_win() public view returns (bool) {
         //rows
         if (cells[0].value == Value.X && cells[1].value == Value.X && cells[2].value == Value.X){
@@ -144,6 +235,7 @@ contract Tris {
             return true;
         }
         
+        //no win condition
         return false;
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,17 @@
 // scripts/deploy.js
 const { ethers } = require("hardhat");
 
+// standard deploy script
 async function main () {
-    const Tris = await ethers.getContractFactory('Tris');
-    tris = await Tris.deploy();
-    [player1, player2] = await ethers.getSigners();
-    await tris.waitForDeployment();
-    console.log('Tris deployed to:', await tris.getAddress());
-    console.log([player1, player2]);
-    
-
-  }
+  // deploy
+  const Tris = await ethers.getContractFactory('Tris');
+  tris = await Tris.deploy("0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
+  accounts = await ethers.getSigners();
+  [owner,player1, player2] = accounts;
+  await tris.waitForDeployment();
+  const events = await tris.queryFilter("*");
+  console.log(events);
+  };
   
   main()
     .then(() => process.exit(0))
