d4e693f10cbd43060903774955d3b7b2a5c08aa6
Alberto <77164768+albynotar@users.noreply.github.com>
2024-09-28 11:24:14 +0200

    added game emulation istance in /scripts/play.js

--- scripts/play.js
+++ scripts/play.js
@@ -1,61 +1,35 @@
-// scripts/functions.js
+// scripts/play.js
 const { ethers } = require("hardhat");
 
+// script to emulate a game instance
 async function main () {
-    const X = await ethers.getContractFactory('Tris');
-    const x = await X.deploy();
-    await x.waitForDeployment();
-    [player1, player2] = await ethers.getSigners();
-    console.log([player1, player2]);
-    //console.log((await x.get_player1()).toString());
-    //console.log((await x.get_player2()).toString());
-    //await x.set_cell_value(0,0);
-    //console.log((await x.get_cell_value(0)).toString());
-    //await x.set_cell_value(4,1);
-    //console.log((await x.get_cell_value(4)).toString());
-    //await x.set_cell_value(8,2);
-    //console.log((await x.get_cell_value(8)).toString());
-    //console.log((await x.get_grid()));
-    //await x.reset_grid();
-    //console.log((await x.get_cell(0)).toString());
-    //console.log((await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",0,1)));
-    //console.log((await x.get_grid()));
-    //await x.set_cell_value(0,0);
-    //await x.reset_grid();
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",0,1);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",1,1);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",2,1);
-    //console.log((await x.check_win()));
-    //await x.reset_grid();
-    //console.log((await x.get_grid()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",0,1);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",1,2);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",2,1);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",3,1);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",4,2);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",5,1);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",6,2);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",7,1);
-    //console.log((await x.check_win()));
-    //await x.move("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",8,2);
-    //console.log((await x.check_win()));
-    await x.reset_grid();
-    await x.move(player1,0,1);
-    await x.move(player1,4,1);
-    await x.move(player1,8,1);
-    //console.log((await x.get_grid()));
-    //await x.reset_grid();
-    //console.log((await x.get_grid()));
+    // deploy
+    const Tris = await ethers.getContractFactory('Tris');
+    tris = await Tris.deploy("0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
+    accounts = await ethers.getSigners();
+    [owner,player1, player2] = accounts;
+    await tris.waitForDeployment();
+    await tris.connect(player1).move(0);
+    console.log(await tris.connect(player1).get_grid());
+    await tris.connect(player2).move(1);
+    console.log(await tris.connect(player2).get_grid());
+    await tris.connect(player1).move(2);
+    console.log(await tris.connect(player1).get_grid());
+    await tris.connect(player2).move(3);
+    console.log(await tris.connect(player2).get_grid());
+    await tris.connect(player1).move(4);
+    console.log(await tris.connect(player1).get_grid());
+    await tris.connect(player2).move(5);
+    console.log(await tris.connect(player1).get_grid());
+    await tris.connect(player1).move(7);
+    console.log(await tris.connect(player2).get_grid());
+    await tris.connect(player2).move(8);
+    console.log(await tris.connect(player1).get_grid());
+    await tris.connect(player1).move(9);
+    console.log(await tris.connect(player1).get_grid());
+    console.log(await tris.connect(player1).get_move_count());
+    const events = await tris.queryFilter("*");
+    console.log(events);
   }
   
   main()
