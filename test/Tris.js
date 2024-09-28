e8f0636f08913c5170318789d6c330fad96afd5b
Alberto <77164768+albynotar@users.noreply.github.com>
2024-09-28 11:21:50 +0200

    Added tests in /test/Tris.js

--- test/Tris.js
+++ test/Tris.js
@@ -1,18 +1,39 @@
+// scripts/Tris.js
+const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe('Tris', function() {
-  let tris, player1, player2;
-
   beforeEach(async () => {
     const Tris = await ethers.getContractFactory('Tris');
-    tris = await Tris.deploy();
-    [player1, player2] = await ethers.getSigners();
+    tris = await Tris.deploy("0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
+    accounts = await ethers.getSigners();
+    [owner,player1, player2] = accounts;
     await tris.waitForDeployment();
-    console.log([player1, player2]);
   });
 
-  it('check that player can play', async function() {
-    console.log("aa");
+  it('correctly execute a valid move', async function() {
+      before_move = parseInt(await tris.connect(player1).get_cell(0)[0]);
+      await tris.connect(player1).move(0);
+      after_move = parseInt(await tris.connect(player1).get_cell(0)[0]);
+      await expect(before_move == 0 && after_move == 1);
+      await tris.connect(owner).reset_grid();
+  });
+  
+  it('correctly revert a move given that the caller is not an autorized player', async function() {
+    await expect(tris.connect(accounts[3]).move(0)).to.be.revertedWith('msg.sender is not an autorized player');
+  });
+  
+  it('correctly revert a move given that the cell is already assigned', async function() {
+    await tris.connect(player1).move(0);
+    await expect(tris.connect(player2).move(0)).to.be.revertedWith('Cell is already assigned');
+    await tris.connect(owner).reset_grid();
+  });
+
+  it('correctly revert an illegal move', async function() {
+    await expect(tris.connect(player2).move(0)).to.be.revertedWith('Player 2 attempted a move outside of his turn');
+    await tris.connect(player1).move(0);
+    await expect(tris.connect(player1).move(1)).to.be.revertedWith('Player 1 attempted a move outside of his turn');
+    await tris.connect(owner).reset_grid();
   });
 
 });
